2ba56029a935c1f120dd683ebfb112039f8bcf78
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2023-05-05 17:11:38 +0800

    Setup php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'no_unused_imports' => true,
+])
+    ->setFinder($finder);
